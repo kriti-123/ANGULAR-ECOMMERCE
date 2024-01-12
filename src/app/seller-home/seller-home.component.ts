d860f1a4c476b8738d116606cbc99517c6684e8d
kriti-123 <ks11115500@gmail.com>
2024-01-12 14:04:43 +0530

    display product list

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrl: './seller-home.component.css'
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent{
+  productListArr: undefined|product[];
+  constructor(private product:ProductService){
+  // ngOnInIt(){
+    this.product.productList().subscribe((result)=>{
+      console.log(result,"products list here");
+      this.productListArr = result;
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import {product} from '../data-type';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,10 @@ import { Router } from '@angular/router';
 export class ProductService {
 
   constructor(private http:HttpClient,private router:Router) { }
-  addProduct(data:any){
+  addProduct(data:product){
    return this.http.post('http://localhost:3000/products',data);
+  }
+  productList(){
+    return this.http.get<product[]>('http://localhost:3000/products');
   }
 }
